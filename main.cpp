b7eb8e450b63030270e4c557e7e691df8ba47fd2
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-09-16 10:28:36 -0500

    4 con aumentarPrecioPorcentaje

--- main.cpp
+++ main.cpp
@@ -3,34 +3,66 @@
 
 using namespace std;
 
-class Estudiante {
-public:
+class Producto {
+private:
     string nombre;
-    int id;
-    int edad;
-    Estudiante() : nombre( "sin nombre"), id(0), edad(0) {}
-    Estudiante(string nom) : nombre(nom), id(0), edad(0) {};
-    Estudiante(string nom, int i, int d) : nombre(nom), id(i), edad(d) {};
-    ~Estudiante() {
-        cout << "Destruyendo el objeto" << endl;
+    int cantidad;
+    int precio;
+
+public:
+    int getPrecio() const { return precio; }
+    int getCantidad() const { return cantidad; }
+    string getNombre() const { return nombre; }
+    void setNombre(const string& nom) { nombre = nom; };
+
+    void setCantidad(int cant) {
+        if (cant < 0) {
+            cout << "Bruto, como se le ocurre" << endl;
+            return;
+        }
+        cantidad = cant;
     }
+
+    void setPrecio(int p) { precio = p; }
+
+    void aumentarPrecio(int valor) {
+        precio += valor;
+    }
+
+    void aumentarPrecioPorcentaje(float porc) {
+        precio *= (1 + porc / 100.0);
+    }
+
+    Producto() : nombre("sin nombre"), cantidad(0), precio(0) {
+    }
+
+    Producto(string nom, int cant, int p) : nombre(nom), cantidad(cant), precio(p) {
+    }
+
     void mostrar() {
-        cout << "[ " << nombre << ", " << id << ", " << edad << " ]" << endl;
-        return;
-    }
-    void cumple() {
-        edad++;
+        cout << "[nombre=" << nombre << ", cantidad=" << cantidad << ", precio=" << precio << "]" << endl;
     }
 };
 
-
 int main() {
-    Estudiante a("pedro", 100, 17), b("maria", 200, 18);
-    {
-        Estudiante x;
-        x.mostrar();
-    }
+    Producto a("papas", 40, 100);
+    Producto b("churros", 10, 2000);
     a.mostrar();
     b.mostrar();
+    cout << a.getPrecio() << endl;
+    b.setPrecio(4000);
+    b.mostrar();
+    a.setCantidad(-100);
+    a.mostrar();
+    b.mostrar();
+    a.aumentarPrecio(40);
+    b.aumentarPrecio(40);
+    a.mostrar();
+    b.mostrar();
+    a.aumentarPrecioPorcentaje(10);
+    b.aumentarPrecioPorcentaje(80);
+    a.mostrar();
+    b.mostrar();
+
     return 0;
 }
