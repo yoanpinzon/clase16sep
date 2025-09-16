ed7b39699cc069cc69ed8117e44df09973b4b652
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-09-16 09:54:01 -0500

    3 destructor dentro de un bloque

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ public:
     Estudiante() : nombre( "sin nombre"), id(0), edad(0) {}
     Estudiante(string nom) : nombre(nom), id(0), edad(0) {};
     Estudiante(string nom, int i, int d) : nombre(nom), id(i), edad(d) {};
-    ~Estudiante() { }
+    ~Estudiante() {
+        cout << "Destruyendo el objeto" << endl;
+    }
     void mostrar() {
         cout << "[ " << nombre << ", " << id << ", " << edad << " ]" << endl;
         return;
@@ -24,17 +26,11 @@ public:
 
 int main() {
     Estudiante a("pedro", 100, 17), b("maria", 200, 18);
-    Estudiante otro( "tola", 1000, 500 ), c( "carlos");
+    {
+        Estudiante x;
+        x.mostrar();
+    }
     a.mostrar();
-    b.mostrar();
-    otro.mostrar();
-    c.mostrar();
-    b.edad++;
-    a.mostrar();
-    b.mostrar();
-    a.cumple();
-    a.mostrar();
-    for (int i = 0; i < 5; i++ ) { b.cumple(); }
     b.mostrar();
     return 0;
 }
